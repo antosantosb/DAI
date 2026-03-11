11816bc045fa266e0cf409f30e613d5248e6c615
kevgoncalves <kevgoncalves123@gmail.com>
2026-03-11 00:16:19 +0000

    Spring boot adicionado e docker-compose.yml atualizado

---
+++ pgu/src/main/java/dai/tub/pgu/PguApplication.java
@@ -0,0 +1,13 @@
+package dai.tub.pgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PguApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PguApplication.class, args);
+	}
+
+}
---
+++ pgu/src/test/java/dai/tub/pgu/PguApplicationTests.java
@@ -0,0 +1,13 @@
+package dai.tub.pgu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PguApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
