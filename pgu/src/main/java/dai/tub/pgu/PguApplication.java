b990dd90ac55e31b72b4882c8d9dd1bb40106d88
kevgoncalves <kevgoncalves123@gmail.com>
2026-03-11 15:51:39 +0000

    Small changes

--- pgu/src/main/java/dai/tub/pgu/PguApplication.java
+++ pgu/src/main/java/dai/tub/pgu/PguApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PguApplication {
+public class PguApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(PguApplication.class, args);
 	}
 
---
+++ pgu/src/main/java/dai/tub/pgu/config/MqttConfig.java
@@ -0,0 +1,87 @@
+package dai.tub.pgu.config;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.messaging.Message;
+
+@Configuration
+public class MqttConfig 
+{
+    // Vai buscar as variáveis do file application.properties
+    @Value("${mqtt.broker.url}")
+    private String brokerUrl;
+
+    @Value("${mqtt.client.id}")
+    private String clientId;
+
+    @Value("${mqtt.topics.telemetry}")
+    private String telemetryTopic;
+
+    // Configuração da ligação ao Mosquitto
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory()
+    {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        
+        MqttConnectOptions options = new MqttConnectOptions();
+        
+        // Configuração das options
+        options.setServerURIs(new String[] {brokerUrl}); // Dá set no URI com o brokerUrl
+        options.setCleanSession(true);
+
+        // Utiliza as options criadas, na factory
+        factory.setConnectionOptions(options); 
+
+        return factory;
+    }
+
+    // Criação do canal de entrada
+    @Bean
+    public MessageChannel mqttInputChannel()
+    {
+        return new DirectChannel();
+    } 
+
+    // Criação da escuta "Listener"
+    @Bean
+    public MessageProducer inBound()
+    {
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(clientId, mqttClientFactory(), telemetryTopic);
+        
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1); // Qualidade de serviço 1 (Garante a entrega)
+        adapter.setOutputChannel(mqttInputChannel());
+
+        return adapter;
+    }
+
+    // O que fazer ao receber uma mensagem do Mosquitto
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public org.springframework.messaging.MessageHandler handler()
+    {
+        return new org.springframework.messaging.MessageHandler()
+        {
+            @Override
+            public void handleMessage(Message<?> message)
+            {
+                System.out.println("====================");
+                System.out.println("Novo autocarro recebido!");
+                System.out.println("Tópico: " + message.getHeaders().get("mqtt_receivedTopic"));
+                System.out.println("Payload: " + message.getPayload());
+                System.out.println("====================");
+            }
+        };
+    }
+}
